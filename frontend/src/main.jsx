16daceb0cbd58ba4499f28b901e9b5b6e4b8c206
helen1789 <146372680+helen1789@users.noreply.github.com>
2026-09-01 02:45:54 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,6 @@
+import './styles/fonts.css'
+import './styles/tokens.css'
+import './styles/global.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
