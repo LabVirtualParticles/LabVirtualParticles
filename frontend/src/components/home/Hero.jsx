153207f4e70e5e444952079d9939fe5b9f264133
lorenzo <chesslorenzo9@gmail.com>
2026-09-09 11:04:15 -0300

    Canvas1.1

--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import HeroCanvasFX from './HeroCanvasFX';
 import './Hero.css';
 
 export default function Hero() {
@@ -17,8 +16,6 @@ export default function Hero() {
 
   return (
     <section className="hero">
-      <HeroCanvasFX />
-
       <div className="hero__content">
         <h1 className="hero__title">Geantino Labs</h1>
         <p className="hero__lede">
